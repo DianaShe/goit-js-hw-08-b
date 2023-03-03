ba250f0f590b8b352797149f2323fd794751d0af
DianaShe <dikravchyk@gmail.com>
2023-03-03 21:22:33 +0200

    Edited Task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,9 +5,8 @@ const form = document.querySelector('.feedback-form');
 form.addEventListener('input', throttle(onInputChange, 500));
 form.addEventListener('submit', onFormSubmit);
 
-const savedData = JSON.parse(localStorage.getItem("feedback-form-state"));
 const formData = {};
-
+const savedData = JSON.parse(localStorage.getItem("feedback-form-state"));
 populateInputData();
 
 function onInputChange(event) {
@@ -23,7 +22,12 @@ function onFormSubmit(event) {
 }
 
 function populateInputData() {
-    savedData.email ? form.elements.email.value = savedData.email : form.elements.email.value = "";
-    savedData.message ? form.elements.message.value = savedData.message : form.elements.message.value = "";
+    if (savedData && savedData.email) {
+        form.elements.email.value = savedData.email;
+    };
+
+    if (savedData && savedData.message) {
+        form.elements.message.value = savedData.message;
+    } 
    
 }
