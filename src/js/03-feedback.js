2eaed079658d03cfbac2f374a9397704e7e002ec
DianaShe <dikravchyk@gmail.com>
2023-03-04 23:03:03 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,13 +5,18 @@ const form = document.querySelector('.feedback-form');
 form.addEventListener('input', throttle(onInputChange, 500));
 form.addEventListener('submit', onFormSubmit);
 
-let formData = {};
-
 populateInputData();
 
+
 function onInputChange(event) {
-    if (JSON.parse(localStorage.getItem("feedback-form-state"))) {
-        formData = JSON.parse(localStorage.getItem("feedback-form-state"));
+    const formData = {};
+    const storedData = localStorage.getItem("feedback-form-state");
+    
+    if (storedData) {
+        formData.email = form.elements.email.value;
+    }
+    if (storedData) {
+        formData.message = form.elements.message.value;
     }
     
     formData[event.target.name] = event.target.value;
@@ -21,12 +26,14 @@ function onInputChange(event) {
 function onFormSubmit(event) {
     event.preventDefault();
     console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
-    form.reset();
-    localStorage.removeItem("feedback-form-state"); 
+    event.currentTarget.reset();
+    localStorage.removeItem("feedback-form-state");
 }
+
 
 function populateInputData() {
     const savedData = localStorage.getItem("feedback-form-state");
+
     if (savedData && JSON.parse(savedData).email) {
         form.elements.email.value = JSON.parse(savedData).email;
     };
